23c6ba72514a1b0bb44d27f5c838bc5fad36da9e
eloy6lega <eloy.perez@campusfp.es>
2024-01-26 08:57:45 +0100

    react-componentes

---
+++ src/Clientes.jsx
@@ -0,0 +1,33 @@
+export default function Clientes() {
+  return (
+    <div>
+      <h1>Clientes</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Apellido</th>
+            <th>Ciudad</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Pedro Sanchez</td>
+            <td>Madrid</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Eloy Perez</td>
+            <td>Barcelona</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>Pablo Gomez</td>
+            <td>Valencia</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
